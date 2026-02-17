81a3a5f210850961f76f4766e574fe59db81aa85
Brian Munyoki <brianmunyoki168@gmail.com>
2026-02-17 23:03:19 +0300

    button function

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,12 @@
 import react from 'react';
 
-function button(){
+function button({label, onClick,className,type='button'}){
     return(
-        <>
-        
-        </>
+        <button
+        type={type}
+        onClick={onClick}
+        className={`btn ${className}`}>
+        { label}
+        </button>
     )
 }
