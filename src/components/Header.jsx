675ac0b661bb5ce4c2778302a25d0f6f7bd2986e
Brian Munyoki <brianmunyoki168@gmail.com>
2026-02-17 22:58:57 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <>
+       <h1>Welcome Back</h1>
+       <div>Please select your role to continue</div> 
+       </>
+    );
+}
+
+export default Header;
