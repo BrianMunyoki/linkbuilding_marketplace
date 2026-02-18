2e5471fe6769c33867552565f031bb53aa4c51e9
Brian Munyoki <brianmunyoki168@gmail.com>
2026-02-18 07:39:21 +0300

    creating login form

---
+++ src/components/form.jsx
@@ -0,0 +1,26 @@
+import React ,{useState} from 'react';
+
+function form({onSubmit}){
+//states for inputs
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+//handle input changes
+const handleEmailChange=(e)=>setEmail(e.target.value);
+const handlePasswordChange=(e)=>setPassword(e.target.value);
+//handle form submission
+const handleSubmit=(e)=>{
+    e.preventDefault();
+    onSubmit({email,passowrd});
+};
+    return(
+        <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email address:</label><br />
+        <input type="text" name="email" value={email} onChange={handleEmailChange} placeholder="Enter your email" /><br />
+        <label htmlFor="password">password:</label><br />
+        <input type="password" name="password" id="password" value={password} onChange={handlePasswordChange} placeholder="Enter your password" /><br />
+        <button type="submit">Submit</button>
+        </form>
+    );
+}
+
+export default form;
